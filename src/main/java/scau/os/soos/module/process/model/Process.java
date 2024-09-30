5bde788bf0dcdd829eed9c8350fd27d033f1946e
oujia-kai <2239998048@qq.com>
2024-10-01 00:40:44 +0800

    -

--- src/main/java/scau/os/soos/module/process/model/Process.java
+++ src/main/java/scau/os/soos/module/process/model/Process.java
@@ -1,30 +1,16 @@
 package scau.os.soos.module.process.model;
 
-import scau.os.soos.common.enums.DEVICE_TYPE;
-
 public class Process {
     // 进程控制块
     private PCB pcb;
 
-    // 正在使用设备类型
-    private DEVICE_TYPE deviceType;
-
     public Process(PCB pcb, int pid) {
         this.pcb = pcb;
         this.pcb.initPCB(pid,this);
-        this.deviceType = null;
     }
 
     public PCB getPCB() {
         return pcb;
-    }
-
-    public void setDeviceType(DEVICE_TYPE deviceType) {
-        this.deviceType = deviceType;
-    }
-
-    public DEVICE_TYPE getDeviceType() {
-        return deviceType;
     }
 
 }
