069d8fd250a765ddd96a467c37b25ced1e647d13
nanven <icedeerwanna@outlook.com>
2024-10-27 23:57:09 +0800

    修复：任务栏被窗口阻挡 && 新增：打开任务管理器

---
+++ src/main/java/scau/os/soos/apps/taskManager/TaskManagerApp.java
@@ -0,0 +1,19 @@
+package scau.os.soos.apps.taskManager;
+
+import scau.os.soos.ui.components.base.Window;
+
+public class TaskManagerApp extends Window {
+    public TaskManagerApp() {
+        super("任务管理器", "main.fxml", 900, 560);
+    }
+
+    @Override
+    protected void initialize() {
+
+    }
+
+    @Override
+    protected void close() {
+
+    }
+}
